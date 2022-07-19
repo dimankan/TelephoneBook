83b80b24d424e38afabfeaf5085889802eecd88e
dimankan <dimankan94@mail.ru>
2022-07-19 22:28:07 +0300

    Добавил сетку (грид), добавил кнопки удалить, добавить, просмотреть, найти

--- TelephoneBook/Form1.Designer.cs
+++ TelephoneBook/Form1.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btSelectAll = new System.Windows.Forms.Button();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btAddContact = new System.Windows.Forms.Button();
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(378, 233);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btSelectAll
+            // 
+            this.btSelectAll.Location = new System.Drawing.Point(437, 28);
+            this.btSelectAll.Name = "btSelectAll";
+            this.btSelectAll.Size = new System.Drawing.Size(117, 43);
+            this.btSelectAll.TabIndex = 1;
+            this.btSelectAll.Text = "Считать с БД";
+            this.btSelectAll.UseVisualStyleBackColor = true;
+            this.btSelectAll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(52, 348);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(258, 23);
+            this.tbNumber.TabIndex = 2;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(52, 393);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(258, 23);
+            this.tbName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Номер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 375);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // btAddContact
+            // 
+            this.btAddContact.Location = new System.Drawing.Point(336, 361);
+            this.btAddContact.Name = "btAddContact";
+            this.btAddContact.Size = new System.Drawing.Size(104, 42);
+            this.btAddContact.TabIndex = 4;
+            this.btAddContact.Text = "Добавить контакт";
+            this.btAddContact.UseVisualStyleBackColor = true;
+            this.btAddContact.Click += new System.EventHandler(this.btAddContact_Click);
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(465, 154);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(233, 23);
+            this.tbFind.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(580, 193);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(118, 57);
+            this.btDelete.TabIndex = 7;
+            this.btDelete.Text = "Удалить найденные контакты";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbFind);
+            this.Controls.Add(this.btAddContact);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.btSelectAll);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btSelectAll;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btAddContact;
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btDelete;
     }
 }
--- TelephoneBook/Form1.cs
+++ TelephoneBook/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,5 +18,76 @@ namespace TelephoneBook
             InitializeComponent();
         }
 
+        MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Database=telephonebook;Uid=root;Pwd=root;");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter("select * from accounts", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void btAddContact_Click(object sender, EventArgs e)
+        {
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter($"INSERT INTO `accounts`(`Name`, `Phone`) VALUES ('{tbName.Text}','{tbNumber.Text}')", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+            }
+
+            button1_Click(sender, e);
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter($"select * from accounts where name = '{tbFind.Text}' or Phone = '{tbFind.Text}' ", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Удаляем??", "Удаление", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+            if(dialog == DialogResult.OK)
+            {
+                using (conn)
+                {
+                    MySqlDataAdapter SDA = new MySqlDataAdapter($"DELETE FROM `accounts` WHERE Phone = '{tbFind.Text}' or Name = '{tbFind.Text}' ", conn);
+                    DataTable dataTable = new DataTable();
+                    SDA.Fill(dataTable);
+                }
+
+                button1_Click(sender, e);
+            }
+
+        }
+
+
     }
+
+
 }
+
+
+#region Trash
+/*
+ List<Model> models = new List<Model>();
+            models.Add(new Model() { Id = 1, Name = "Misha"});
+            models.Add(new Model() { Id = 2, Name = "Dima"});
+
+            dataGridView1.DataSource = models;
+ */
+
+#endregion
